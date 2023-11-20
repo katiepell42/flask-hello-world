1645d546b7833091d5b2adec3fffa6cf0b715cea
Katie Pell <57158239+katiepell42@users.noreply.github.com>
2023-11-19 18:34:49 -0600

    committing

--- app.py
+++ app.py
@@ -1,31 +1,50 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
+import random
 
 app = Flask(__name__)
+app.secret_key = 'supersecretkey'
 
-# Sample initial tasks
-tasks = [{'name': 'Task 1', 'completed': False}, {'name': 'Task 2', 'completed': False},
-         {'name': 'Task 3', 'completed': False}]
+class HangmanGame:
+    def __init__(self):
+        self.word_list = ["python", "hangman", "programming", "computer", "keyboard", "interface"]
+        self.word_to_guess = random.choice(self.word_list)
+        self.guesses_left = 6
+        self.guessed_letters = set()
+        self.wrong_letters = set()
 
-@app.route('/')
+    def display_word(self):
+        return " ".join([letter if letter in self.guessed_letters else "_" for letter in self.word_to_guess])
+
+    def make_guess(self, guess):
+        if guess.isalpha() and len(guess) == 1:
+            if guess in self.guessed_letters:
+                flash("You already guessed that letter.")
+            else:
+                self.guessed_letters.add(guess)
+                if guess not in self.word_to_guess:
+                    self.wrong_letters.add(guess)
+                    self.guesses_left -= 1
+
+                if self.guesses_left == 0:
+                    self.game_over("You lose! The word was " + self.word_to_guess)
+                elif set(self.word_to_guess) <= self.guessed_letters:
+                    self.game_over("Congratulations! You guessed the word.")
+                else:
+                    flash(f"Guesses left: {self.guesses_left}")
+        else:
+            flash("Please enter a valid single letter.")
+
+    def game_over(self, message):
+        flash(message)
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', tasks=tasks)
+    if request.method == 'POST':
+        guess = request.form['guess'].lower()
+        hangman_game.make_guess(guess)
 
-@app.route('/add_task', methods=['POST'])
-def add_task():
-    new_task_name = request.form.get('new_task')
-    new_task = {'name': new_task_name, 'completed': False}
-    tasks.append(new_task)
-    return redirect(url_for('index'))
-
-@app.route('/delete_task/<int:task_id>', methods=['POST'])
-def delete_task(task_id):
-    del tasks[task_id - 1]
-    return redirect(url_for('index'))
-
-@app.route('/toggle_task/<int:task_id>', methods=['POST'])
-def toggle_task(task_id):
-    tasks[task_id - 1]['completed'] = not tasks[task_id - 1]['completed']
-    return redirect(url_for('index'))
+    return render_template('hangman.html', hangman_game=hangman_game)
 
 if __name__ == '__main__':
+    hangman_game = HangmanGame()
     app.run(debug=True)
