5162f7b12dffb4c9efc3df43c6ed5808b6eb9788
Katie Pell <57158239+katiepell42@users.noreply.github.com>
2023-11-19 18:08:24 -0600

    first commit

--- app.py
+++ app.py
@@ -1,18 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('home.html')
-
-@app.route('/gallery')
-def gallery():
-    return render_template('gallery.html')
-
-@app.route('/about')
-def about():
-    return render_template('about.html')
-
-if __name__ == '__main__':
-    app.run(debug=True)
+    return('hello, world!')
