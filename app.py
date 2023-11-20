b3b221dfd8ef4720fd9629988f87703fd32c764d
Katie Pell <57158239+katiepell42@users.noreply.github.com>
2023-11-19 18:17:56 -0600

    committing

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
+# Sample initial tasks
+tasks = ["Task 1", "Task 2", "Task 3"]
+
 @app.route('/')
-def home():
-    return render_template('index.html')
+def index():
+    return render_template('index.html', tasks=tasks)
+
+@app.route('/add_task', methods=['POST'])
+def add_task():
+    new_task = request.form.get('new_task')
+    tasks.append(new_task)
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
