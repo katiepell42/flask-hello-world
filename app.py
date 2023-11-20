8bfd23b141a4842bec8be463fa38dfba8e6e7bce
Katie Pell <57158239+katiepell42@users.noreply.github.com>
2023-11-19 18:21:18 -0600

    committing

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
 # Sample initial tasks
-tasks = ["Task 1", "Task 2", "Task 3"]
+tasks = [{'name': 'Task 1', 'completed': False}, {'name': 'Task 2', 'completed': False},
+         {'name': 'Task 3', 'completed': False}]
 
 @app.route('/')
 def index():
@@ -11,8 +12,19 @@ def index():
 
 @app.route('/add_task', methods=['POST'])
 def add_task():
-    new_task = request.form.get('new_task')
+    new_task_name = request.form.get('new_task')
+    new_task = {'name': new_task_name, 'completed': False}
     tasks.append(new_task)
+    return redirect(url_for('index'))
+
+@app.route('/delete_task/<int:task_id>', methods=['POST'])
+def delete_task(task_id):
+    del tasks[task_id - 1]
+    return redirect(url_for('index'))
+
+@app.route('/toggle_task/<int:task_id>', methods=['POST'])
+def toggle_task(task_id):
+    tasks[task_id - 1]['completed'] = not tasks[task_id - 1]['completed']
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
